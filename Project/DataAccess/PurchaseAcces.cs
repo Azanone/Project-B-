760aa9d9b28245ea799492d717ac00957e6752fe
Baset-1102300 <1102300@hr.nl>
2026-04-06 19:58:27 +0200

    Add PurchaseAcces data access class
    Introduce Project/DataAccess/PurchaseAcces.cs: a simple data access class using Microsoft.Data.Sqlite and Dapper. It declares a SqliteConnection pointing to DataSources/project.db, a Table = "Purchase" field, and a GetAll() method that queries the Purchase table and returns a List<PurchaseModel>.

---
+++ Project/DataAccess/PurchaseAcces.cs
@@ -0,0 +1,17 @@
+using Microsoft.Data.Sqlite;
+
+using Dapper;
+
+public class PurchaseAcces
+{
+    private SqliteConnection _connection = new SqliteConnection($"Data Source=DataSources/project.db");
+
+    private string Table = "Purchase";
+
+    public List<PurchaseModel> GetAll()
+    {
+        string sql = $"SELECT * FROM {Table}";
+        return _connection.Query<PurchaseModel>(sql).ToList();
+    }
+
+}
