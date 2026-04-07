8af1ef05783f9becf4c8f77a7e4f9beae2a2126b
Baset-1102300 <1102300@hr.nl>
2026-04-07 14:14:05 +0200

    Created Showproduct presenation

---
+++ Project/Presentation/ShowProduct.cs
@@ -0,0 +1,27 @@
+public static class ShowProducts
+{
+    static public void Start()
+    {
+        Console.WriteLine("Enter 1 to return to Admin Menu");
+        string input = Console.ReadLine();
+        if (input == "1")
+        {
+            AdminMenu.Start();
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+            Start();
+        }
+    }
+
+    public static void ShowAll()
+    {
+        var list = accountsLogic.GetPurchases();
+        Console.WriteLine("--- ALL PRODUCTS ---");
+        foreach (var item in list)
+        {
+            Console.WriteLine($"ID: {item.PurchaseID}| Name: {item.UserName}| Date:-{item.PurchaseDate}| Amount:{item.TotalAmount}");
+        }
+    }
+}
