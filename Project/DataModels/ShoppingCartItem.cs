b5bd2accbb3bcac4cfb4c9bbfbf6469d791d6428
JairoWarner <91611684+JairoWarner@users.noreply.github.com>
2026-04-08 18:13:58 +0200

    added ShoppingCartItem and better version of ShoppingListLogic

---
+++ Project/DataModels/ShoppingCartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Project.Models;
+
+public class ShoppingCartItem
+{
+    public ShoppingListModel Product { get; }
+    public int Quantity { get; set; }
+
+    public ShoppingCartItem(ShoppingListModel product, int quantity)
+    {
+        Product = product;
+        Quantity = quantity;
+    }
+}
