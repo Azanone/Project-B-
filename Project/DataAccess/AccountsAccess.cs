ed110f7346da5285c6023eb4a6b656072455440a
JairoWarner <91611684+JairoWarner@users.noreply.github.com>
2026-04-14 19:45:36 +0200

    lokale wijziginge

--- Project/DataAccess/AccountsAccess.cs
+++ Project/DataAccess/AccountsAccess.cs
@@ -1,38 +1,35 @@
-using Microsoft.Data.Sqlite;
+    using Microsoft.Data.Sqlite;
 
 using Dapper;
 
 
 public class AccountsAccess
 {
-    private SqliteConnection _connection = new SqliteConnection($"Data Source=DataSources/project.db");
+    private SqliteConnection _connection = DBconnection._c;
 
-    private string Table = "User";
+    private string Table = "\"USER\"";
 
     public void Write(AccountModel account)
     {
-        string sql = $"INSERT INTO {Table} (email, password, fullname) VALUES (@EmailAddress, @Password, @FullName)";
+        string sql = $"INSERT INTO {Table} (Name, Username, EmailAddress, Password, FullName, Postcode, HouseNumber, phoneNumber, Role) VALUES (@Username, @Username, @EmailAddress, @Password, @FullName, @Postcode, @HouseNumber, @phoneNumber, @Role)";
         _connection.Execute(sql, account);
     }
 
-    public AccountModel GetByEmail(string email)
+    public AccountModel? GetByIdentifier(string identifier)
     {
-        string sql = $"SELECT * FROM {Table} WHERE email = @Email";
-        return _connection.QueryFirstOrDefault<AccountModel>(sql, new { Email = email });
+        string sql = $"SELECT UserID AS Id, CASE WHEN lower(Username) = 'admin' OR lower(EmailAddress) = 'admin' THEN 'Admin' ELSE 'User' END AS Role, Username, EmailAddress, Password, FullName, Postcode, HouseNumber, phoneNumber FROM {Table} WHERE EmailAddress = @Identifier OR Username = @Identifier";
+        return _connection.QueryFirstOrDefault<AccountModel>(sql, new { Identifier = identifier });
     }
 
     public void Update(AccountModel account)
     {
-        string sql = $"UPDATE {Table} SET email = @EmailAddress, password = @Password, fullname = @FullName WHERE id = @Id";
+        string sql = $"UPDATE {Table} SET Name = @FullName, Username = @Username, EmailAddress = @EmailAddress, Password = @Password, FullName = @FullName, Postcode = @Postcode, HouseNumber = @HouseNumber, phoneNumber = @phoneNumber WHERE UserID = @Id";
         _connection.Execute(sql, account);
     }
 
     public void Delete(AccountModel account)
     {
-        string sql = $"DELETE FROM {Table} WHERE id = @Id";
+        string sql = $"DELETE FROM {Table} WHERE UserID = @Id";
         _connection.Execute(sql, new { Id = account.Id });
     }
-
-
-
 }
