f46d2aff7b6eeef4a4a7e45a6d7852829ffd8fa1
Baset-1102300 <180225053+Baset-1102300@users.noreply.github.com>
2026-05-18 23:37:29 +0200

    Update ShowFinancialOverview.cs

--- Project/Presentation/ShowFinancialOverview.cs
+++ Project/Presentation/ShowFinancialOverview.cs
@@ -3,7 +3,9 @@ public static class ShowFinancialOverview
     public static void Start()
     {
         MenuHelpers.Announce("--- FINANCIAL OVERVIEW ---\n");
+        MenuHelpers.Confirm("Enter 1 to return to Admin Menu\n ");
         MenuHelpers.Confirm("Choose date range (DD-MM-YYYY): ");
+
         string? firstDate = MenuHelpers.Prompt("");
         if (firstDate == "1")
         {
@@ -19,6 +21,25 @@ public static class ShowFinancialOverview
             Start();
             return;
         }
+        if (int.Parse(dates[1]) < 1 || int.Parse(dates[1]) > 12 || int.Parse(dates2[1]) < 1 || int.Parse(dates2[1]) > 12)
+        {
+            MenuHelpers.Warn("Invalid month. Please use a month between 01 and 12.");
+            Start();
+            return;
+        }
+        if (int.Parse(dates[0]) < 1 || int.Parse(dates[0]) > 31 || int.Parse(dates2[0]) < 1 || int.Parse(dates2[0]) > 31)
+        {
+            MenuHelpers.Warn("Invalid day. Please use a day between 01 and 31.");
+            Start();
+            return;
+        }
+        if (int.Parse(dates[2]) < 2000 || int.Parse(dates[2]) > DateTime.Now.Year || int.Parse(dates2[2]) < 2000 || int.Parse(dates2[2]) > DateTime.Now.Year)
+        {
+            MenuHelpers.Warn($"Invalid year. Please use a year between 2000 and {DateTime.Now.Year}.");
+            Start();
+            return;
+        }
+        
         try
         {
             DateTime startDate = new DateTime(int.Parse(dates[2]), int.Parse(dates[1]), int.Parse(dates[0]));
